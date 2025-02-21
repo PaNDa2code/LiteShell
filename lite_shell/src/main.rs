11b1d9620db9814c43254d48f363aea32bd3a5dd
PaNDa2code <moaaz0688@gmail.com>
2025-02-20 21:55:21 -0500

    chore: rename project to LiteShell

--- lite_shell/src/main.rs
+++ lite_shell/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{Read, Write};
 use console_session::ConsoleSession;
 
 fn main() -> std::io::Result<()> {
-    let console = ConsoleSession::new_shell(console_session::ShellApp::Bash)?;
+    let console = ConsoleSession::new_shell(console_session::ShellApp::CMD)?;
 
     let mut stdin = console.stdin_write_file;
     let mut stdout = console.stdout_read_file;
